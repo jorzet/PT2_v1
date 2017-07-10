f15d9b4958cdb09430e04fe62f8b814ad13b7498
joge zepeda <jorzet.94@gmail.com>
2017-07-10 15:27:34 -0500

    implementing a new animation in list view

--- app/src/main/java/com/eeg/pt1_v1/ui/activities/BluetoothConnectionActivity.java
+++ app/src/main/java/com/eeg/pt1_v1/ui/activities/BluetoothConnectionActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.eeg.pt1_v1.R;
 
 /**
- * Created by ing_ragde on 09/07/17.
+ * Created by Jorge Zepeda Tinoco on 09/07/17.
  */
 
 public class BluetoothConnectionActivity extends AppCompatActivity {
@@ -14,6 +14,6 @@ public class BluetoothConnectionActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.container);
+        setContentView(R.layout.bluetooth_container);
     }
 }
